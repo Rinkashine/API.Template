d5ca632737a580ae20b4782845a10fc749eb1b5c
Mark Manalo <markjosephmanalo1110@gmail.com>
2025-03-10 11:20:31 +0800

    Add Automapper File

---
+++ API.Template/Mapping/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Template.Domain.DTO;
+using Template.Domain.Entities;
+
+namespace API.Template.Mapping
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            //Category
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<AddCategoryRequestDTO, Category>().ReverseMap();
+            CreateMap<UpdateCategoryRequestDto, Category>().ReverseMap();
+
+        }
+       
+    }
+}
